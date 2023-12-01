fe48cf3179b3bd95aa295fb98daed58c05364596
1ntizor <intizorpayrav@gmail.com>
2023-12-01 14:34:58 +0500

    my Rest API

---
+++ src/main/java/RestAPIConsumer/restAPIConsumer/App.java
@@ -0,0 +1,13 @@
+package RestAPIConsumer.restAPIConsumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
